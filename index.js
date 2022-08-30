e5924144ace1fda7bf6312b56f254e524204fbc7
joeygarber <joseph.z.garber@gmail.com>
2022-08-30 14:20:21 -0400

    Got createTest working, started hooking things up to API

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const auth = require('./lib/auth')
 
 // define server and client ports
 const serverDevPort = 4741
-const clientDevPort = 7165
+const clientDevPort = 3000
 
 // establish database connection
 // use new version of URL parser
---
+++ seeds.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const Test = require('./app/models/test')
+
+const db = require('./config/db')
+
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('Connection successful')
+})
+.catch((err) => {
+  console.log(err)
+})
+
+const seedTests = [
+  {
+    title: 'Lions',
+    body: 'Lions are cute and you should for sure try to pet one if you come across one.'
+  },
+  {
+    title: 'Rules of Baseball',
+    body: 'Baseball has no rules. It is a chaos sport.'
+  }
+]
+
+const seedDB = async () => {
+  await Test.deleteMany({})
+  await Test.insertMany(seedTests)
+}
+
+seedDB().then(() => {
+  mongoose.connection.close()
+})
+.then(() => console.log('closed'))
